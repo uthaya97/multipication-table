630b2be1a396dd9f27cc1447399de9f3b2813c09
paulkani <paulkani.s@golden-hippo.com>
2023-07-19 19:25:54 +0530

    multi

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-var n=prompt("enter n value");
-var a=2;
+var a=prompt("which number multipication table");
+var d=prompt("enter limit of multipication");
 var b=1;
 var c;
-while(b<=n)
+while(b<=d)
       {
 		c=b*a;
 		document.write(b+"x"+a+"="+c+"<br>");
 		b++;
 	
       }
+	  
+	  
+	// 2 x multipication table
+	
+	  // var n=prompt("enter n value");
+// var a=2;
+// var b=1;
+// var c;
+// while(b<=n)
+      // {
+		// c=b*a;
+		// document.write(b+"x"+a+"="+c+"<br>");
+		// b++;
+	
+      // }
